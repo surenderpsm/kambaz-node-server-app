095fd0d94f5c3c7d0ad9cb533f7d5ac6ebd637c3
Dheeraj <dheerajreddy.in@gmail.com>
2025-04-02 22:43:59 -0400

    1st commit

---
+++ Lab5/QueryParameters.js
@@ -0,0 +1,28 @@
+export default function QueryParameters(app) {
+    app.get("/lab5/calculator", (req, res) => {
+        const { a, b, operation } = req.query;
+        let result = 0;
+        switch (operation) {
+            case "add":
+                result = parseInt(a) + parseInt(b);
+                break;
+            case "subtract":
+                result = parseInt(a) - parseInt(b);
+                break;
+            case "multiply":
+                result = parseInt(a) * parseInt(b);
+                break;
+            case "divide":
+                if (b == 0) {
+                    res.status(400).send("Error: Division by zero is not allowed");
+                    return;
+                }
+                result = parseInt(a) / parseInt(b);
+                break;
+            default:
+            result = "Invalid operation";
+        }
+        res.send(result.toString());
+    });
+}
+  
---
+++ index.js
@@ -0,0 +1,47 @@
+import "dotenv/config";
+import express from 'express';
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5/index.js";
+import CourseRoutes from "./Kambaz/Courses/routes.js";
+import cors from "cors";
+import UserRoutes from "./Kambaz/Users/routes.js";
+import session from "express-session";
+const app = express();
+
+app.use(
+    cors({
+        credentials: true,
+        origin: process.env.NETLIFY_URL || "http://localhost:5173",
+    })
+);
+ // make sure cors is used right after creating the app
+const sessionOptions = {
+    secret: process.env.SESSION_SECRET || "kambaz",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+        domain: process.env.NODE_SERVER_DOMAIN,
+    };
+}
+app.use(session(sessionOptions));
+
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+app.use(
+    session(sessionOptions)
+);
+app.use(express.json());
+UserRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
+
+
